6b115549fbffe0ee01b494edaed8bf36884ffb29
SafaAlatrech <alatrechsafa@gmail.com>
2020-08-30 19:48:21 +0200

    first commit

---
+++ SampleApp/Migrations/20200416185250_azz.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleApp.Migrations
+{
+    public partial class azz : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Adresse",
+                table: "Students");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Adresse",
+                table: "Students",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ SampleApp/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace SampleApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+       
+
+        
+        
+
+    }
+}
